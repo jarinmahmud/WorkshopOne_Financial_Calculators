0d5c2ee6064eddda530f47a0987031288c41a78f
Jarin Mahmud <jarinmahmud@gmail.com>
2024-04-12 16:14:33 -0400

    friday commit

--- src/main/java/com/jm/Main.java
+++ src/main/java/com/jm/Main.java
@@ -1,8 +1,38 @@
 package com.jm;
 
 
+import java.sql.SQLOutput;
+import java.util.*;
+
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //takes principal, interest rate and loan length as input
+        //output would be monthly payment and total interest paid
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        double principal = scanner.nextDouble();
+
+        System.out.println("Interest Rate: ");
+        double interest_rate = scanner.nextDouble();
+
+        System.out.println("Loan Length");
+        double loan_length = scanner.nextDouble();
+
+        System.out.println("Number of times interest compounded: ");
+        double n = scanner.nextDouble();
+
+        double total_compound_interest = principal *(pow ((1 + (interest_rate/n)), (loan_length * n)));
+        double monthly_compound_interest =  total_compound_interest - principal;
+
+        System.out.println("Total Compound Interest is " + total_compound_interest + ".");
+        System.out.println("Monthly Compound Interest is " + monthly_compound_interest + ".");
+
+
     }
+
+
 }
